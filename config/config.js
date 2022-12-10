a7d9a127e83006eda5fee337a043aec1c489db05
Muhammad Arhum <67513459+arhumsharif@users.noreply.github.com>
2022-12-10 17:28:58 +0500

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,113 @@
+const mysql = require("mysql");
+const env = require("./env-config.json");
+const env_server = require("./env-config-server.json");
+//create connection
+// port: env.dbhost.port,
+// local is false
+let local = false;
+let vars;
+if (local == true) {
+  vars = {
+    host: env.dbhost.host,
+    user: env.dbhost.user,
+    password: env.dbhost.password,
+    database: env.dbhost.database,
+  };
+} else {
+  vars = {
+    host: env_server.dbhost.host,
+    user: env_server.dbhost.user,
+    password: env_server.dbhost.password,
+    database: env_server.dbhost.database,
+  };
+}
+
+var colors          = require('colors');
+var q               = require('q');
+var MySQLConnection = {};
+
+MySQLConnection.connect = function(){
+    var d = q.defer();
+    MySQLConnection.connection = mysql.createConnection(vars);
+
+    MySQLConnection.connection.connect(function (err) {
+        if(err) {
+            console.log('Not connected '.red, err.toString().red, ' RETRYING...'.blue);
+            d.reject();
+        } else {
+            console.log('Connected to Mysql. Exporting..'.blue);
+            d.resolve(MySQLConnection.connection);
+        }
+    });
+    return d.promise;
+};
+
+MySQLConnection.createPool = function(){
+  MySQLConnection.pool = mysql.createPool(vars);
+}
+
+MySQLConnection.createPool();
+MySQLConnection.connect().then(function(con){
+  console.log('connected!');
+  let mysql1 = con;
+  mysql1.on('error', function (err, result) {
+    console.log('error occurred. Reconneting...'.purple);
+    MySQLConnection.reconnect();
+  });
+});
+
+MySQLConnection.Query = function(thisQuery, func){
+  console.log(thisQuery.green)
+  let p = new Promise((resolve)=>{
+    MySQLConnection.pool.getConnection(function(err, con){
+      console.log('connected!');
+      con.query(thisQuery, function(err, result){
+        if(err){
+          con.release();
+          resolve({err:err, result:null})
+        }
+        con.release();
+        resolve({err:null, result:result})
+      });
+    });
+  });
+  return p.then((data)=>{return data;});
+}
+
+MySQLConnection.Query1 = function(thisQuery, func){
+  console.log(thisQuery.green)
+  let p = new Promise((resolve)=>{
+    MySQLConnection.connect().then(function(con){
+      console.log('connected!');
+      let mysql1 = con;
+      mysql1.on('error', function (err, result) {
+        console.log('error occurred. Reconneting...'.purple);
+        MySQLConnection.reconnect();
+      });
+      mysql1.query(thisQuery, function(err, result){
+        if(err){
+          resolve({err:err, result:null})
+        }
+        resolve({err:null, result:result})
+      });
+    });
+  });
+  return p.then((data)=>{return data;});
+}
+
+MySQLConnection.reconnect = function(){
+  console.log("reconnecting".red);
+    MySqlConnection.connect().then(function(con){
+      console.log("connected. getting new reference");
+        let mysql1 = con;
+        mysql1.on('error', function (err, result) {
+          console.log("Trying Again!!".red);
+            MySQLConnection.reconnect();
+        });
+    }, function (error) {
+      console.log("try again");
+        setTimeout(MySQLConnection.reconnect, 2000);
+    });
+};
+
+module.exports = MySQLConnection;
---
+++ modules/auth/authenticate.js
@@ -0,0 +1,105 @@
+const db = require("./../../config/config");
+const env1 = require("./../../config/env-config.json");
+var md5 = require("md5");
+const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+// Secret Key generator
+function getSecretKey() {
+  var randomChars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var result = "";
+  for (var i = 0; i < 8; i++) {
+    result += randomChars.charAt(
+      Math.floor(Math.random() * randomChars.length)
+    );
+  }
+  return result;
+}
+// authenticate
+router.post("/",async (req, res) => {
+  let role = 0
+  let adminEmail = "admin@gmail.com"
+  let adminPass = md5('123456')
+  // for checking if it ia a admin
+  let password = req.body.Password;
+  password = password;
+  let sql = `SELECT * FROM users where email = ${db.connection.escape(
+    req.body.Email
+  )} AND password = ${db.connection.escape(md5(password))}`;
+  let quedminry = db.Query(sql).then(({err, result}) => {
+
+    if(err){
+      return res.status(401).json({
+        message: "Auth Failed1!",
+      });
+    }
+
+    if (result.length < 1) {
+      res.status(401).json({
+        message: "Auth Failed1!",
+      });
+      return;
+    }
+    if (result) {
+      const secretKey = getSecretKey();
+      let secretKeyQuery = `UPDATE users SET secret_key = ${db.connection.escape(
+        secretKey
+      )} WHERE user_guid=${db.connection.escape(result[0].user_guid)}`;
+      let updateSecretKey = db.Query(secretKeyQuery).then(({err:err1, result:response1}) => {
+        console.log(err1, response1)
+        if (err1) {
+          res.status(401).json({
+            message: "Auth Failed!2",
+          });
+          return;
+        }
+
+        const token = jwt.sign(
+          {
+            user_guid: result[0].user_guid,
+            email: result[0].email,
+          },
+          secretKey,
+          {
+            expiresIn: "1h",
+          }
+        );
+
+        if (result[0].role == 'admin')
+        {
+          return res.status(200).json({
+            message: "Auth Successfull",
+            token: token + " " + result[0].user_guid,
+            role: 1
+          });    
+        }
+
+        // Checking if user is verified using AWS
+        // if (results[0].is_verified == 0)
+        // {
+        //   return res.status(200).json({
+        //     message: "Auth Successfull",
+        //     verification: 'aws_failed',
+        //     token: token + " " + results[0].user_guid,
+        //   })
+        // }
+
+
+        res.status(200).json({
+          message: "Auth Successfull",
+          token: token + " " + result[0].user_guid,
+          role: 0
+        });
+        return;
+      });
+    } else {
+      res.status(401).json({
+        message: "Auth Failed!4",
+      });
+      return;
+    }
+  });
+});
+module.exports = router;
