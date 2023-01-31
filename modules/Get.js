dbc804c63053c7374847b268341e5f9bc29b3c0a
Abdullah Tariq <mat57557@gmail.com>
2023-01-31 16:31:18 +0500

    added viewAllTourGuides Route

--- modules/Get.js
+++ modules/Get.js
@@ -105,4 +105,20 @@ router.get('/package/view/all', (req, res) => {
   });
 });
 
+router.get('/tourguide/view/all', (req, res) => {
+  let sql1 = `SELECT * FROM tour_guide`;
+  let query1 = db.query(sql1, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: 'Some Error Occured in Checking',
+      });
+    }
+    return res.status(200).json({
+      message: 'Success',
+      data: result,
+    });
+  });
+});
+
 module.exports = router;
